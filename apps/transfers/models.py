75fdff43b30ede2d6974ee30ef0090624c734ddd
Aydar2007 <brawlball22@mail.ru>
2023-09-11 17:12:53 +0600

    Модели Transfers

--- apps/transfers/models.py
+++ apps/transfers/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+from apps.users.models import Authentication
+
+class Transfers(models.Model):
+    who_transfer = models.ForeignKey(
+        Authentication,
+        on_delete=models.CASCADE,
+        related_name="Who_transfer",
+        verbose_name="Кто отправил"
+    )
+    who_get = models.ForeignKey(
+        Authentication,
+        on_delete=models.CASCADE,
+        related_name="who_get",
+        verbose_name="Получатель"
+    )
+    is_completed = models.BooleanField(
+        default=False,
+        verbose_name="Статус"
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Произведен'
+    )
+    how_much = models.CharField(
+        max_length=99999,
+        verbose_name="Сумма"
+    )
+    def __str__(self):
+        return self.who_transfer
+    
+    class Meta:
+        verbose_name = "Истории"
+        verbose_name_plural = "История"
